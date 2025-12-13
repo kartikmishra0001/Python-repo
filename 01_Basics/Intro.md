f275c676ba6c915b3b6064d4c9a4c27f130cbdf6
kartikmishra0001 <mishrakartik672@gmail.com>
2025-12-13 12:00:59 +0530

    Initial commit

---
+++ 01_Basics/Intro.md
@@ -0,0 +1,18 @@
+## What is Python?
+
+Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+### It is used for:
+
+web development (server-side),
+software development,
+mathematics,
+system scripting.
+
+### Example 
+`print("Hello, World!")`
+
+## Start 
+Python is an interpreted programming language, this means that as a developer you write Python (.py) files in a text editor and then put those files into the python interpreter to be executed.
+
+Make a file Intro.py and try to print "hello world"
---
+++ 01_Basics/Intro.py
@@ -0,0 +1,21 @@
+# Python Syntax
+print("Hello World")
+
+# Python Variable
+# In Python, variables are created when you assign a value to it:
+
+x = 5
+y = "Hello, World"
+
+# Comments
+# print(" Comment ")
+
+# Python Statements
+# A computer program is a list of "instructions" to be "executed" by a computer.
+# In a programming language, these programming instructions are called statements.
+
+# Print Without a New Line
+# If you want to print multiple words on the same line, you can use the end parameter:
+
+print("Hello World", end=" ")
+print("I am in same line")
